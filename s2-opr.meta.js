c31d66d488f328adbf8ecb9da299928aa0c19a91
Adail Horst <the.spaww@gmail.com>
2018-07-27 10:33:31 -0300

    Create s2-opr.meta.js

---
+++ s2-opr.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id             iitc-plugin-s2-opr
+// @name           IITC plugin: Portal Submission helper
+// @author         SpawW (based on Vib S17 plugin)
+// @category       Layer
+// @version        0.0.1
+// @namespace      https://github.com/vibrunazo/l17cells
+// @updateURL      https://raw.githubusercontent.com/SpawW/s2-opr/master/s2-opr.meta.js
+// @downloadURL    https://raw.githubusercontent.com/SpawW/s2-opr/master/s2-opr.js
+// @description    IITC: Shows cells on the map for help submission planing
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
